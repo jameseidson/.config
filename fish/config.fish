d52f314dcf592f0c4fafff125f63778dd6d53567
jameseidson <eidsonje@icloud.com>
2023-07-10 23:28:12 -0600

    fish: add detach function

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
 end
 
 abbr -a ssh "TERM=xterm-256color ssh -Y" 
-abbr -a new "alacritty &> /dev/null & disown"
+abbr -a new "detach alacritty"
+
+function detach 
+  $argv &> /dev/null &; disown
+end
 
 alias nvim="SHELL=/bin/bash /bin/nvim"
