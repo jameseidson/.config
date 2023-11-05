78ed1a8a498d170df31cd5e43c9ebeaf7cdb6340
jameseidson <eidsonje@icloud.com>
2023-11-05 11:37:18 -0700

    zellij: use alt for keybinds + more ergonomics

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-abbr -a ssh "TERM=xterm-256color ssh -Y" 
+abbr -a ssh "TERM=xterm-256color ssh -Y"
 abbr -a new "detach alacritty"
 
-function detach 
-  $argv &> /dev/null &; disown
+alias nvim="SHELL=/bin/bash /bin/nvim"
+
+function detach
+    $argv &>/dev/null & disown
 end
 
-alias nvim="SHELL=/bin/bash /bin/nvim"
+function bash-source
+    exec bash -c "source $argv; exec fish"
+end
